a98815e09958cdbb5c23857ddeacda9e8e391f83
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-04-04 20:13:15 +0530

    Id card

---
+++ taking_input.py
@@ -0,0 +1,29 @@
+# Input 
+
+# String input 
+# name = input("name : ")
+# print("My name is ",name)
+
+# int input
+
+# age = int(input("age : "))
+# print("My age is ",age)
+
+# float input 
+
+# cgpa = float(input("cgpa : "))
+# print("My CGPA is ",cgpa)
+
+# Project - ID Card
+# Create a simple ID card program that takes user input for various fields and displays the information in a formatted manner.
+
+student_id = [
+	input("Whats your Name : "),
+	input("Whats your Course : "),
+	input("Whats your Domain : "),
+	input("Whats your College name : "),
+	input("Any Working Experience : "), 
+	input("You are Student T/F : ")
+]
+
+print(student_id)
